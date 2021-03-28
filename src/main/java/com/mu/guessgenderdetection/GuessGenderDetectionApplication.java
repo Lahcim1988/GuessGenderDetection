9c82108b55ae79077f70370477757b64968599d7
Lahcim1988 <michalurbanek1988@gmail.com>
2021-03-28 20:28:47 +0200

    initial commit

---
+++ src/main/java/com/mu/guessgenderdetection/GuessGenderDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.mu.guessgenderdetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessGenderDetectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuessGenderDetectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mu/guessgenderdetection/GuessGenderDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mu.guessgenderdetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessGenderDetectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
