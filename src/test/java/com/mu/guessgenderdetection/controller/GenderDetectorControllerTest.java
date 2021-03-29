3b0a8eecfcd39465734a77d3ea1d5365537734bd
Lahcim1988 <michalurbanek1988@gmail.com>
2021-03-29 20:01:17 +0200

    add few tests

--- src/test/java/com/mu/guessgenderdetection/controller/GenderDetectorControllerTest.java
+++ src/test/java/com/mu/guessgenderdetection/controller/GenderDetectorControllerTest.java
@@ -1,18 +1,11 @@
 package com.mu.guessgenderdetection.controller;
 
 import com.mu.guessgenderdetection.enumeration.GenderEnum;
-import com.mu.guessgenderdetection.service.GenderListImpl;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-
-import java.util.List;
-
-import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -25,14 +18,26 @@ class GenderDetectorControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    GenderListImpl genderList;
-
     @Test
     public void shouldReturnMaleGenderForV1() throws Exception {
 
-        this.mockMvc.perform(get("/gender/guess/Jacob/1"))
+        this.mockMvc.perform(get("/gender/guess/Jacob Samantha Grace/1"))
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString(String.valueOf(GenderEnum.MALE))));
+    }
+
+    @Test
+    public void shouldReturnFemaleGenderForV1() throws Exception {
+        this.mockMvc.perform(get("/gender/guess/Anna Jacob/1"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString(String.valueOf(GenderEnum.FEMALE))));
+    }
+
+    @Test
+    public void shouldReturnInconclusiveGenderForV1() throws Exception {
+        this.mockMvc.perform(get("/gender/guess/NoName/1"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString(String.valueOf(GenderEnum.INCONCLUSIVE))));
     }
 
     @Test
@@ -44,12 +49,18 @@ class GenderDetectorControllerTest {
     }
 
     @Test
-    public void shouldReturnAllTokens() throws Exception{
-        List list = genderList.allNames();
+    public void shouldReturnInconclusiveGenderForV2() throws Exception {
 
-        this.mockMvc.perform(get("/gender/all"))
-                .andExpect((ResultMatcher) list);
+        this.mockMvc.perform(get("/gender/guess/Jacob Samantha"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString(String.valueOf(GenderEnum.INCONCLUSIVE))));
     }
 
+    @Test
+    public void shouldReturnInconclusiveGenderForV2NoNames() throws Exception {
 
+        this.mockMvc.perform(get("/gender/guess/This is not a name"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString(String.valueOf(GenderEnum.INCONCLUSIVE))));
+    }
 }
